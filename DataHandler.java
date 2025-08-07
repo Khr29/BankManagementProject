34c79b0cb74f1078bd8e65eae501dc9541a2ccc3
khaled <khaled.taha.ahmed12@gmail.com>
2025-08-07 15:43:33 +0530

    init

--- DataHandler.java
+++ DataHandler.java
@@ -4,4 +4,17 @@ import java.util.ArrayList;
 public class DataHandler {
 
     private static final String FILE_NAME ="accounts.txt";
+
+    public static void saveAccount(ArrayList<Account> accounts){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME))){
+            for(Account acc : accounts){
+                writer.write(acc.toFileString());
+                writer.newLine();
+            }
+            System.out.println("Accounts Saved To File.");
+        } catch(IOException e){
+            System.out.println("Error saving accounts: "+ e.getMessage());
+        }
+    }
+    
 }
