c2c0bf1ac45ab29e0daa12267dfee0941b431108
khaled <khaled.taha.ahmed12@gmail.com>
2025-08-07 16:14:50 +0530

    init

--- DataHandler.java
+++ DataHandler.java
@@ -17,12 +17,29 @@ public class DataHandler {
         }
     }
 
-    public static ArrayList<Account>loadAccounts(){
+    public static ArrayList<Account> loadAccounts(){
         ArrayList<Account> accounts = new ArrayList<>();
         File file = new File(FILE_NAME);
-        if(!file.exists());
+        if(!file.exists()){
         return accounts;
+        
     }
     
+    try(BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))){
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split(",");
+            int accNum = Integer.parseInt(parts[0]);
+            String name = parts[1];
+            String password = parts[2];
+            double balance = Double.parseDouble(parts[3]);
+            Account account =new Account(accNum, name, password, balance);
+            accounts.add(account);
+        }
+    } catch (IOException e){
+        System.out.println("Error Loading Accounts: "+ e.getMessage());
+    }
+    return accounts;
 
+    }
 }
